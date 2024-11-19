93cf8df7b314e061da9a72e10e56ff08e40073b3
Rafael Silva Inacio <rafaelsilvainaciobr@gmail.com>
2024-11-19 14:33:43 -0300

    Update index.php

--- index.php
+++ index.php
@@ -8,7 +8,7 @@ $pageTitle = "Home - B-Code";
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title><?php echo $pageTitle; ?></title>
-    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="main.css">
     <!-- Incluindo o Bootstrap para estilos responsivos -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet">
     <!-- Ícones do Font Awesome -->
