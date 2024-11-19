7df4023929baa408905d5b6996d3d9685e8a1b0f
Rafael Silva Inacio <rafaelsilvainaciobr@gmail.com>
2024-11-19 15:02:44 -0300

    Update main.js

--- main.js
+++ main.js
@@ -1,1 +1,23 @@
 
+gsap.from(".hero-title", {
+        duration: 1.5,
+        opacity: 0,
+        y: -50,
+        ease: "power3.out"
+    });
+
+    gsap.from(".hero-description", {
+        duration: 1.5,
+        opacity: 0,
+        y: 50,
+        delay: 0.5,
+        ease: "power3.out"
+    });
+
+    gsap.from(".hero-btn", {
+        duration: 1.5,
+        opacity: 0,
+        scale: 0.8,
+        delay: 1,
+        ease: "power3.out"
+    });
