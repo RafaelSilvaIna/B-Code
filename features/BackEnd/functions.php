113f62bba2beaac711b10eedad0b8e241eda034d
Rafael Silva Inacio <rafaelsilvainaciobr@gmail.com>
2024-11-19 17:49:17 -0300

    Create functions.php

---
+++ features/BackEnd/functions.php
@@ -0,0 +1,9 @@
+<?php
+// Define a mensagem principal
+$mainMessage = "Obrigado por querer nos ajudar!";
+$secondaryMessage = "Por enquanto, a funcionalidade de doação será implementada em breve, mas você pode nos ajudar de outras formas:";
+
+// Exibe as mensagens
+echo "<h1>$mainMessage</h1>";
+echo "<p>$secondaryMessage</p>";
+?>
